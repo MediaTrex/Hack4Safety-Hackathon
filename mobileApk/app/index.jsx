cc14e179f03c69e35aadbecf9e02a0c93e624d16
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-08 17:05:38 +0545

    completed initial setup with nativewind(tailwindcss)

---
+++ mobileApk/app/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+    return (
+        <View>
+            <Text className="text-red-500 bg-amber-200">Hello Expo Router 🚀 ghaurav</Text>
+        </View>
+    );
+}

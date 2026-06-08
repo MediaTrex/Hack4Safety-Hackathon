cfe0934460061540aaf260a1694340364af4f574
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-09 01:19:04 +0545

    created custom tab bar for Citizen's screens

--- mobileApk/app/(citizens)/(tabs)/_layout.jsx
+++ mobileApk/app/(citizens)/(tabs)/_layout.jsx
@@ -1,8 +1,12 @@
 import { Tabs } from "expo-router";
+import CustomTabBarCitizen from "../../../components/CustomTabBarCitizen";
 
 export default function CreatorTabs() {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs
+            screenOptions={{ headerShown: false }}
+            tabBar={(props) => <CustomTabBarCitizen {...props} />}
+        >
             <Tabs.Screen name="home" />
             <Tabs.Screen name="map" />
             <Tabs.Screen name="sos" />
